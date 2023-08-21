982318a5c1af90fcb1ca3e6d62dcad89496b6442
machengyong <chengyong.ma1@pactera.com>
2023-08-21 17:03:20 +0800

    FIX: 调整代码

--- app/src/main/java/com/example/biometricdemo/MainActivity.kt
+++ app/src/main/java/com/example/biometricdemo/MainActivity.kt
@@ -222,16 +222,3 @@ class MainActivity : AppCompatActivity() {
         return biometrics
     }
 }
-
-/// Various types of biometric authentication.
-/// Some platforms report specific biometric types, while others report only
-/// classifications like strong and weak.
-internal enum class BiometricType {
-    /// platform API considers to be strong. For example, on Android this
-    /// corresponds to Class 3.
-    STRONG,  /// Any biometric (e.g. fingerprint, iris, or face) on the device that the
-
-    /// platform API considers to be weak. For example, on Android this
-    /// corresponds to Class 2.
-    WEAK
-}
